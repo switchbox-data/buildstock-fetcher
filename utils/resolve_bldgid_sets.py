08ce1815aa1b18e63dbd268485304b14a5fd7ae0
alexhyunminlee <lee3742@purdue.edu>
2025-06-05 16:49:12 +0000

    WIP Add resolve_bldgit_sets function

---
+++ utils/resolve_bldgid_sets.py
@@ -0,0 +1,9 @@
+def resolve_bldgid_sets():
+    """
+    Resolve a list of building IDs into a list of building ID sets.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    resolve_bldgid_sets()
