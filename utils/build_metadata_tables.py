a4cf61f750971517ba8d80786c471c667723d0e0
Alex Lee <lee3742@purdue.edu>
2025-06-14 19:18:35 +0000

    WIP: Add more to parquet downloading script

--- utils/build_metadata_tables.py
+++ utils/build_metadata_tables.py
@@ -8,7 +8,6 @@ with open("buildstock_releases.json") as file:
     data = json.load(file)
 
 data_dir = Path(__file__).parent.parent / "data"
-downloaded_paths = []
 os.makedirs(data_dir, exist_ok=True)
 
 for release_name, release in data.items():
@@ -16,18 +15,38 @@ for release_name, release in data.items():
     res_com = release["res_com"]
     weather = release["weather"]
     release_number = release["release_number"]
+    upgrade_ids = release["upgrade_ids"]
 
-    url = (
-        f"https://oedi-data-lake.s3.amazonaws.com/nrel-pds-building-stock/"
-        f"end-use-load-profiles-for-us-building-stock/{release_year}/"
-        f"{res_com}_{weather}_release_{release_number}/metadata/metadata.parquet"
-    )
-    response = requests.get(url, timeout=30)
-    response.raise_for_status()
+    if release_year == "2021":
+        download_url = (
+            f"https://oedi-data-lake.s3.amazonaws.com/nrel-pds-building-stock/"
+            f"end-use-load-profiles-for-us-building-stock/{release_year}/"
+            f"{res_com}_{weather}_release_{release_number}/metadata/metadata.parquet"
+        )
+        response = requests.get(download_url, timeout=30)
+        response.raise_for_status()
 
-    output_path = data_dir / f"{release_name}_metadata.parquet"
-    with open(output_path, "wb") as file:
-        file.write(response.content)
+        output_path = data_dir / f"{release_name}_baseline.parquet"
+        with open(output_path, "wb") as file:
+            file.write(response.content)
+    elif release_year == "2022":
+        for upgrade_id in upgrade_ids:
+            upgrade_id = int(upgrade_id)
+            if upgrade_id == 0:
+                download_url = (
+                    f"https://oedi-data-lake.s3.amazonaws.com/nrel-pds-building-stock/"
+                    f"end-use-load-profiles-for-us-building-stock/{release_year}/"
+                    f"{res_com}_{weather}_release_{release_number}/metadata/baseline.parquet"
+                )
+            else:
+                ownload_url = (
+                    f"https://oedi-data-lake.s3.amazonaws.com/nrel-pds-building-stock/"
+                    f"end-use-load-profiles-for-us-building-stock/{release_year}/"
+                    f"{res_com}_{weather}_release_{release_number}/metadata/upgrade{upgrade_id:02d}.parquet"
+                )
+                response = requests.get(download_url, timeout=30)
+                response.raise_for_status()
 
-    downloaded_paths.append(output_path)
-    print(url)
+                output_path = data_dir / f"{release_name}_upgrade{upgrade_id:02d}.parquet"
+                with open(output_path, "wb") as file:
+                    file.write(response.content)
